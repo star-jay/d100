b2768f8b6bdb977e058bdd7958a970aa1430835d
Reinjan <reinjan.e@gmail.com>
2022-01-23 23:14:08 +0100

    tweak setup file

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='d100',
     version='0.1.dev0',
-    packages=['src/d100', ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
     license='dowhatyouwantwithit license',
     long_description=open('README.md').read(),
 )
