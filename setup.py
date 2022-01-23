8dfd3c2493f832e801545b77c91427eb535ff901
Reinjan <reinjan.e@gmail.com>
2022-01-23 22:41:50 +0100

    add setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='d100',
+    version='0.1.dev0',
+    packages=['src/d100', ],
+    license='dowhatyouwantwithit license',
+    long_description=open('README.md').read(),
+)
